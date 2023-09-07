ed0a75811f9d7d7a2cfeeec5874886fde10f3da7
JeanCarlosSC <jeancarlosodi@gmail.com>
2023-09-07 05:31:53 -0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,258 @@
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Color colorPrincipal = new Color(60, 78, 120);
+        Color colorGrisOscuro = new Color(80, 80, 80);
+
+        Font fuenteTituloPrincipal = new Font("Tahoma", Font.PLAIN, 24);
+        Font fuenteTitulo = new Font("Calibri (Body)", Font.BOLD, 18);
+        Font fuenteTexto = new Font("Calibri Light", Font.PLAIN, 14);
+
+        Cursor cMano = new Cursor(Cursor.HAND_CURSOR);
+        Border bInferiorAzul = BorderFactory.createMatteBorder(0, 0, 2, 0, colorPrincipal);
+        ImageIcon iFondo = new ImageIcon("resources/images/fondo.png");
+        ImageIcon iLogo = new ImageIcon("resources/images/logo.png");
+        ImageIcon iUsuario2 = new ImageIcon("resources/images/usuario2.png");
+        ImageIcon iClave2 = new ImageIcon("resources/images/clave2.png");
+        ImageIcon iPunto1 = new ImageIcon("resources/images/punto1.png");
+        ImageIcon iFacebook1 = new ImageIcon("resources/images/facebook1.png");
+        ImageIcon iTwitter1 = new ImageIcon("resources/images/twitter1.png");
+        ImageIcon iYoutube1 = new ImageIcon("resources/images/youtube1.png");
+        ImageIcon iSvg1 = new ImageIcon("resources/images/imagen1.png");
+        ImageIcon iCerrar = new ImageIcon("resources/images/cerrar.png");
+
+        JFrame ventana = new JFrame("Componentes básicos de la interfaz gráfica");
+
+        JPanel pIzquierdo = new JPanel();
+        pIzquierdo.setBounds(0, 0, 853, 720); // coordenadas y tamaño
+        pIzquierdo.setBackground(Color.WHITE);
+        pIzquierdo.setLayout(null);
+        ventana.add(pIzquierdo);
+
+        JPanel pDerecho = new JPanel();
+        pDerecho.setBounds(853, 0, 427, 720);
+        pDerecho.setBackground(Color.WHITE);
+        pDerecho.setLayout(null);
+        ventana.add(pDerecho);
+
+        ImageIcon iDimAux = new ImageIcon(
+                iLogo.getImage()
+                        .getScaledInstance(40, 40, Image.SCALE_AREA_AVERAGING)
+        );
+
+        JLabel lLogo = new JLabel();
+        lLogo.setBounds(50, 20, 40, 40);
+        lLogo.setIcon(iDimAux);
+        pIzquierdo.add(lLogo);
+
+        JLabel lTituloApp = new JLabel("Login de usuario");
+        lTituloApp.setBounds(100, 18, 220, 40);
+        lTituloApp.setForeground(Color.WHITE);
+        lTituloApp.setFont(fuenteTituloPrincipal);
+        pIzquierdo.add(lTituloApp);
+
+        iDimAux = new ImageIcon(
+                iSvg1.getImage()
+                        .getScaledInstance(600, 414, Image.SCALE_AREA_AVERAGING)
+        );
+
+        JLabel lSvg1 = new JLabel();
+        lSvg1.setBounds(150, 120, 600, 414);
+        lSvg1.setIcon(iDimAux);
+        pIzquierdo.add(lSvg1);
+
+        JLabel lEslogan = new JLabel("Te ayudamos con todo"); // etiqueta de texto
+        lEslogan.setBounds(138, 110, 150, 20);
+        lEslogan.setForeground(colorGrisOscuro); // cambiar el color de la fuente
+        lEslogan.setHorizontalAlignment(JLabel.CENTER); // centrar con respecto al ancho
+        lEslogan.setFont(fuenteTexto);
+        pDerecho.add(lEslogan);
+
+        JLabel lTituloLogin = new JLabel("Registra tus datos");
+        lTituloLogin.setBounds(110, 140, 200, 30);
+        lTituloLogin.setForeground(colorGrisOscuro); // cambiar el color de la fuente
+        lTituloLogin.setHorizontalAlignment(JLabel.CENTER);
+        lTituloLogin.setFont(fuenteTitulo);
+        pDerecho.add(lTituloLogin);
+
+        JLabel lNotificaciones = new JLabel("¿Desea recibir notificaciones?");
+        lNotificaciones.setBounds(100, 400, 200, 20);
+        lNotificaciones.setForeground(colorGrisOscuro);
+        lNotificaciones.setFont(fuenteTexto);
+        lNotificaciones.setHorizontalAlignment(JLabel.CENTER);
+        pDerecho.add(lNotificaciones);
+
+        iDimAux = new ImageIcon(
+                iUsuario2.getImage()
+                        .getScaledInstance(30, 30, Image.SCALE_AREA_AVERAGING)
+        );
+
+        JLabel lUsuario = new JLabel();
+        lUsuario.setBounds(40, 200, 30, 30);
+        lUsuario.setIcon(iDimAux);
+        pDerecho.add(lUsuario);
+
+        iDimAux = new ImageIcon(
+                iClave2.getImage()
+                        .getScaledInstance(30, 30, Image.SCALE_AREA_AVERAGING)
+        );
+
+        JLabel lClave = new JLabel();
+        lClave.setBounds(40, 270, 30, 30);
+        lClave.setIcon(iDimAux);
+        pDerecho.add(lClave);
+
+        JTextField tNombreUsuario = new JTextField("Nombre de usuario");
+        tNombreUsuario.setBounds(80, 200, 260, 40);
+        tNombreUsuario.setForeground(colorPrincipal);
+        tNombreUsuario.setBackground(Color.WHITE);
+        tNombreUsuario.setCaretColor(colorGrisOscuro); // Color de la línea que parpadea
+        tNombreUsuario.setBorder(bInferiorAzul);
+        tNombreUsuario.setHorizontalAlignment(JTextField.CENTER);
+        pDerecho.add(tNombreUsuario);
+
+        JPasswordField tClaveUsuario = new JPasswordField("Clave usuario");
+        tClaveUsuario.setBounds(80, 250, 260, 40);
+        tClaveUsuario.setForeground(colorPrincipal);
+        tClaveUsuario.setBackground(Color.WHITE);
+        tClaveUsuario.setCaretColor(colorGrisOscuro);
+        tClaveUsuario.setBorder(bInferiorAzul);
+        tClaveUsuario.setHorizontalAlignment(JPasswordField.CENTER);
+        pDerecho.add(tClaveUsuario);
+
+        JComboBox<String> cbTipoUsuario = new JComboBox<>();
+        cbTipoUsuario.addItem("Cliente");
+        cbTipoUsuario.addItem("Cajero");
+        cbTipoUsuario.addItem("Administrador");
+        cbTipoUsuario.setBounds(120, 330, 180, 30);
+        cbTipoUsuario.setForeground(colorPrincipal);
+        cbTipoUsuario.setBackground(Color.WHITE);
+        ((JLabel) cbTipoUsuario.getRenderer()).setHorizontalAlignment(JLabel.CENTER);
+        pDerecho.add(cbTipoUsuario);
+
+        JButton bEntrar = new JButton("Entrar");
+        bEntrar.setBounds(110,490, 200, 40);
+        bEntrar.setFocusable(false);
+        bEntrar.setBackground(colorPrincipal);
+        bEntrar.setForeground(Color.white);
+        bEntrar.setCursor(cMano);
+        pDerecho.add(bEntrar);
+
+        iDimAux = new ImageIcon(
+                iCerrar.getImage()
+                        .getScaledInstance(30, 30, Image.SCALE_AREA_AVERAGING)
+        );
+
+        JButton bCerrar = new JButton();
+        bCerrar.setBounds(350, 10, 45, 30);
+        bCerrar.setFocusable(false);
+        bCerrar.setBackground(Color.BLUE);
+        bCerrar.setForeground(Color.WHITE);
+        bCerrar.setCursor(cMano);
+        bCerrar.setIcon(iDimAux);
+        bCerrar.setContentAreaFilled(false);
+        bCerrar.setFocusable(false);
+        bCerrar.setBorder(null);
+        pDerecho.add(bCerrar);
+
+        iDimAux = new ImageIcon(
+                iPunto1.getImage()
+                        .getScaledInstance(20, 20, Image.SCALE_AREA_AVERAGING)
+        );
+
+        JButton bOpcion1 = new JButton();
+        bOpcion1.setBounds(32, 250, 30, 20);
+        bOpcion1.setCursor(cMano);
+        bOpcion1.setIcon(iDimAux);
+        bOpcion1.setContentAreaFilled(false);
+        bOpcion1.setFocusable(false);
+        bOpcion1.setBorder(null);
+        pIzquierdo.add(bOpcion1);
+
+        JButton bOpcion2 = new JButton();
+        bOpcion2.setBounds(32, 300, 30, 20);
+        bOpcion2.setCursor(cMano);
+        bOpcion2.setIcon(iDimAux);
+        bOpcion2.setContentAreaFilled(false);
+        bOpcion2.setFocusable(false);
+        bOpcion2.setBorder(null);
+        pIzquierdo.add(bOpcion2);
+
+        JButton bOpcion3 = new JButton();
+        bOpcion3.setBounds(32, 350, 30, 20);
+        bOpcion3.setCursor(cMano);
+        bOpcion3.setIcon(iDimAux);
+        bOpcion3.setContentAreaFilled(false);
+        bOpcion3.setFocusable(false);
+        bOpcion3.setBorder(null);
+        pIzquierdo.add(bOpcion3);
+
+        iDimAux = new ImageIcon(
+                iFacebook1.getImage()
+                        .getScaledInstance(30, 30, Image.SCALE_AREA_AVERAGING)
+        );
+
+        JLabel lFacebook = new JLabel();
+        lFacebook.setBounds(30, 630, 30, 30);
+        lFacebook.setIcon(iDimAux);
+        lFacebook.setCursor(cMano);
+        pIzquierdo.add(lFacebook);
+
+        iDimAux = new ImageIcon(
+                iTwitter1.getImage()
+                        .getScaledInstance(30, 30, Image.SCALE_AREA_AVERAGING)
+        );
+
+        JLabel lTwitter = new JLabel();
+        lTwitter.setBounds(80, 630, 30, 30);
+        lTwitter.setIcon(iDimAux);
+        lTwitter.setCursor(cMano);
+        pIzquierdo.add(lTwitter);
+
+        iDimAux = new ImageIcon(
+                iYoutube1.getImage()
+                        .getScaledInstance(30, 30, Image.SCALE_AREA_AVERAGING)
+        );
+
+        JLabel lYoutube = new JLabel();
+        lYoutube.setBounds(130, 630, 30, 30);
+        lYoutube.setIcon(iDimAux);
+        lYoutube.setCursor(cMano);
+        pIzquierdo.add(lYoutube);
+
+        iDimAux = new ImageIcon(
+                iFondo.getImage()
+                        .getScaledInstance(720, 720, Image.SCALE_AREA_AVERAGING)
+        );
+
+        JLabel lFondo = new JLabel();
+        lFondo.setBounds(0, 0, 720, 720);
+        lFondo.setIcon(iDimAux);
+        pIzquierdo.add(lFondo);
+
+        JRadioButton checkNo = new JRadioButton("No");
+        checkNo.setBounds(170, 420, 40, 40);
+        checkNo.setFocusable(false);
+        checkNo.setBackground(Color.WHITE);
+        pDerecho.add(checkNo);
+
+        JRadioButton checkSi = new JRadioButton("Si");
+        checkSi.setBounds(220, 420, 40, 40);
+        checkSi.setFocusable(false);
+        checkSi.setBackground(Color.WHITE);
+        pDerecho.add(checkSi);
+
+        ButtonGroup grupo = new ButtonGroup();
+        grupo.add(checkNo);
+        grupo.add(checkSi);
+
+        ventana.setSize(1280, 720); // tamaño
+        ventana.setLayout(null); // layout null
+        ventana.setLocationRelativeTo(null); // centrar ventana
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // para terminar ejecución al cerrar
+        ventana.setVisible(true); // hacer ventana visible
+    }
+}
